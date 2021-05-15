93a88257e4bca91c05b10d1aa2a41447f0260501
Lucas <lucas.varo@ecoles-wis.net>
2021-05-15 12:13:36 +0200

    Localisation de l'utilisateur Centralisation de la carte sur la position de l'utilisateur Appel des nouveaux spot sur l'API aux alentours de l'utilisateur

--- screens/Map.js
+++ screens/Map.js
@@ -1,32 +1,71 @@
 import React, {useEffect, useState} from 'react'
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+
 
 const Map = () => {
 
+  const [loadingStep, setLoadingStep] = useState(true);
   const [loading, setLoading] = useState(true);
+
+
   const [errorLocation, setErrorLocation] = useState();
   const [spot, setSpot] = useState([]);
-  const [longitude, setLongitude] = useState(3.5854814830858133);
-  const [latitude, setLatitude] = useState(43.442214487499285);
+  const [longitude, setLongitude] = useState(5.522865888665046);
+  const [latitude, setLatitude] = useState(43.217965376775766);
+
+  const [northeastCoords, setNortheastCoords] = useState([]);
+  const [southwestCoords, setSouthwestCoords] = useState([]);
+
 
   useEffect(() => {
-    // Récupération des spots dans l'API Sportihome + Stockage dans le state
-    fetch('https://sportihome.com/api/spots/getAllMarkersInBounds/3.717044,43.476744/4.0312790000000005,43.743299900000004',
-    {
-      method: "POST",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body:JSON.stringify({
-        selectedHobbies: ['KITEBOARDING', 'BMX']
+
+    //Récupération de la localisation de l'utilisateur
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      let latitude = location.coords.latitude;
+      let longitude = location.coords.longitude;
+
+      setLatitude(latitude);
+      setLongitude(longitude);
+
+      let northeast = {
+        longitude: longitude + 0.5 / 2,
+        latitude: latitude + 0.5 / 2
+      }
+
+      let southwest = {
+        longitude: longitude - 0.5 / 2,
+        latitude: latitude - 0.5 / 2
+      }
+
+      // Récupération des spots dans l'API Sportihome + Stockage dans le state
+      fetch('https://sportihome.com/api/spots/getAllMarkersInBounds/'+southwest.longitude+','+southwest.latitude+'/'+northeast.longitude+','+northeast.latitude,
+      {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+          selectedHobbies: ['KITEBOARDING', 'BMX']
+        })
       })
-    })
-    .then((response) => response.json())
-    .then((json) => setSpot(json))
-    .catch((error) => console.error(error))
-    .finally(() => setLoading(false));
+      .then((response) => response.json())
+      .then((json) => setSpot(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+
+    })();
+
+
 
   }, []);
 
@@ -50,8 +89,7 @@ const Map = () => {
         <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
-        onRegionChange={()=>{}}
-        region={{latitude: latitude, longitude: longitude, latitudeDelta: 0.5, longitudeDelta: 0.5}}
+        initialRegion={{latitude: latitude, longitude: longitude, latitudeDelta: 0.5, longitudeDelta: 0.5}}
         showsUserLocation={true}
         >
 
